98e777662e341bc52b087b59c3c72d492376e97e
kaloyanpepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2022-02-08 12:45:44 +0000

    Update cardata.js

--- cardata.js
+++ cardata.js
@@ -1,32 +1,36 @@
-const carlist = [
+[
     {
-        brand: "Citroen",
-        model: "Grand C4 Picasso",
-        category: "Minivan",
-        personCapacity: 7,
-        suitcaseCapacity: 4,
-        image: ""
+        "brand": "Citroen",
+        "model": "Grand C4 Picasso",
+        "category": "Minivan",
+        "personCapacity": 7,
+        "suitcaseCapacity": 4,
+        "image": ""
     },
     {
-        brand: "Scoda",
-        model: "Octavia 2020",
-        category: "Small family car",
-        personCapacity: 5,
-        suitcaseCapacity: 2,
-        image: ""
+        "brand": "Scoda",
+        "model": "Octavia 2020",
+        "category": "Small family car",
+        "personCapacity": 5,
+        "suitcaseCapacity": 2,
+        "image": ""
     },
     {
-        brand: "Kia",
-        model: "Sorento",
-        category: "Compact SUV",
-        personCapacity: 5,
-        suitcaseCapacity: 4,
-        image: ""
+        "brand": "Kia",
+        "model": "Sorento",
+        "category": "Compact SUV",
+        "personCapacity": 5,
+        "suitcaseCapacity": 4,
+        "image": ""
 
     },
     {
-        brand: "BMW",
-        model: ""
+        "brand": "BMW",
+        "model": ""
+        "category": "Sport car" 
+        "personCapacity": 5,
+        "suitcaseCapacity": 4,
+        "image"=""
     }
     
 ]
